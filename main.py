89ba10e7485780f8b0766970e073752fe0c15ab4
Bharath  V <118061759+Bharathv91@users.noreply.github.com>
2023-08-23 23:21:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+a = int(input("enter your marks:"))
+if a<35:
+    print("fail")
+elif a>=35 and a<50:
+    print("passed in first class")
+elif a>=50 and a<=80:
+    print(("passed in secomd class"))
+else:
+    print("distinction")
